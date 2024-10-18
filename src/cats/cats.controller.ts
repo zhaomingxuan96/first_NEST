d38393aea9533a925fdc794f9f95ea4f8f2b8225
evin zhao <zhaomingxuan96@126.com>
2024-10-18 17:51:34 +0800

    add: 第一次提交

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,70 @@
+import {
+  Controller,
+  Get,
+  Query,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+
+import {
+  CreateCatDto,
+  UpdateCatDto,
+  ListAllEntities,
+} from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interface/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+
+  @Post('postcat')
+  async createCat(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get('getcats')
+  async findAllCats(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Post('create')
+  create(@Body() createCatDto: CreateCatDto) {
+    if (!createCatDto.name) {
+      // 抛出一个 HTTP 400 Bad Request 异常
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'Name is required',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return { data: createCatDto, message: 'success', status: 201 };
+  }
+
+  @Get('list')
+  findAll(@Query() query: ListAllEntities) {
+    return `This action returns all cats (limit: ${query.limit} items)`;
+  }
+
+  @Get('user:id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    return `This action updates a #${id} cat ${updateCatDto.age}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
+}
---
+++ src/cats/dto/create-cat.dto.ts
@@ -0,0 +1,16 @@
+export class CreateCatDto {
+  name: string;
+  age: number;
+  breed: string;
+}
+
+export class UpdateCatDto {
+  name: string;
+  age: number;
+  breed: string;
+}
+
+export class ListAllEntities {
+  page: number;
+  limit: number;
+}
